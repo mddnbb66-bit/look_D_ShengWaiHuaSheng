c138dddae1c72dc9197d4283efe70ab92a4a57bf
mdd <mddnbb66@gmail.com>
2026-09-13 16:42:24 +0800

    接入JWT签发，使用固定测试用户，待验证阶段

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
-import app from './app.js'
+//加载.env的
+import "dotenv/config";
+import app from "./app.js";
+//加载env文件变量
 
-app.listen(3001,()=>{
-	console.log('服务器运行，');
-})
+app.listen(3001, () => {
+	console.log("服务器运行，");
+});
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,7 +1,16 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
 const router = express.Router();
-
+//jwt
+const JWT_SECRET = process.env.JWT_SECRET || "develpemnt-secret";
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "20s";
+function signToken(user) {
+	return jwt.sign(
+		{ uid: user._id, email: user.email }, //载荷
+		JWT_SECRET, //密钥
+		{ expiresIn: JWT_EXPIRES_IN }
+	); //过期时间
+}
 //登录逻辑
 router.post("/login", (req, res) => {
 	// 读取 authorization 请求头
@@ -15,11 +24,12 @@ router.post("/login", (req, res) => {
 		return res.status(400).json({ code: 40001, message: "邮箱或密码错误" });
 	}
 	if (email === "mddnbb66@gmail.com" && password === "a") {
+		const token = signToken(user);
 		return res.status(200).json({
 			code: 0,
 			message: "登录成功",
 			data: {
-				token: "fake-token",
+				token: token,
 				user: { email },
 			},
 		});
@@ -27,5 +37,14 @@ router.post("/login", (req, res) => {
 		return res.status(401).json({ code: 40101, message: "邮箱或密码错误" });
 	}
 });
-
+//测试jwt相关路由
+const user = { _id: "mdd", email: "mddnbb66@gmail.com" };
+router.get("/testjwt", (req, res) => {
+	const token = signToken(user);
+	console.log(token);
+	res.json({
+		message: "JWT 生成成功",
+		token,
+	});
+});
 export default router;
